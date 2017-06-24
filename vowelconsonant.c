1fc0a825c05fe6c2622e9435fec5806f77b4777f
gowripurushothaman <gowrikalpana27@gmail.com>
2017-06-24 19:40:48 +0530

    Create vowelconsonant.c

---
+++ vowelconsonant.c
@@ -0,0 +1,7 @@
+char c;
+printf("enter an alphabet");
+scanf("%c",&c);
+if (c=='a'||c=='A'||c=='e'||c=='E'||c=='i'||c=='I'||c=='o'||c=='O'||c=='u'||c=='U')
+printf("%c is a vowel",c);
+else
+printf("%c is a consonant",c);
